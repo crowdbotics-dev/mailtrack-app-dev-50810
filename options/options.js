605955be6b2d97fa298044d28e7b7318879db28e
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2022-12-15 16:42:09 +0530

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "mailtrack-app-dev-50810",
   url: "https://mailtrack-app-dev-50810.botics.co",
   api: "https://mailtrack-app-dev-50810.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
